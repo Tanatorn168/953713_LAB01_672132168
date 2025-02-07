27b20536eec6a7c9077f5ddb99916a1b29a1155c
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-07 17:11:08 +0700

    task9_greaternumber

--- for.js
+++ for.js
@@ -1,3 +1,11 @@
 //arrow function description
 const greet = (name) => 'Hello ' + name
 console.log(greet('John'))
+
+//multiple function
+const multiply = (x,y) => {return x*y}
+console.log(multiply(5,10))
+
+//arrow function greater number
+const getMax = (a,b) => {return a > b ? a : b}
+console.log(getMax(13,10))
