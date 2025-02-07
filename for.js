2137b0bfc191b8ce15c07a2518123c6cb1a226de
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-07 11:59:09 +0700

    task8_totalScores

--- for.js
+++ for.js
@@ -1,4 +1,15 @@
+//function declaration
 function greet(name) {
     return 'Hello ' + name
 } 
 console.log(greet('Perterparker'))
+
+//student score
+function sumScores(scores) {
+    let total = 0;
+    for (let i = 0; i < scores.length; i++) {
+        total += scores[i];
+    }
+    return total;
+}
+console.log(sumScores([10, 20, 30]));
