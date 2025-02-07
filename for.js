f8a218779e29cb46b9be652b86244955228b5313
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-07 11:39:50 +0700

    task7_NameList

--- for.js
+++ for.js
@@ -1,4 +1,9 @@
-let color =['red', 'blue', 'green', 'yellow', 'orange', 'purple']
-for (let i = 0; i < color.length; i++) {
-    console.log(color[i])
+let Name = ['james', 'cameron', 'jonathan','david', 'alexander'];
+for (let i = 0; i < Name.length; i++) {
+    if (Name[i].length > 6) {
+        console.log(`สวัสดีคุณ ${Name[i]}`);
+    } 
+    else {
+        console.log(`Hello ${Name[i]}`);
+    }
 }
