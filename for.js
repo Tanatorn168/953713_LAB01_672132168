83924fde57e208a729232a3f19c479b547546659
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-02-07 11:52:44 +0700

    task8_hellopeterparker

--- for.js
+++ for.js
@@ -1,9 +1,4 @@
-let Name = ['james', 'cameron', 'jonathan','david', 'alexander'];
-for (let i = 0; i < Name.length; i++) {
-    if (Name[i].length > 6) {
-        console.log(`สวัสดีคุณ ${Name[i]}`);
-    } 
-    else {
-        console.log(`Hello ${Name[i]}`);
-    }
-}
+function greet(name) {
+    return 'Hello ' + name
+} 
+console.log(greet('Perterparker'))
